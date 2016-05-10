94fad57007dc4496901d72b59b266ad4eb279b54
Viktor Gevel <viktorgevel@gmail.com>
2016-05-10 16:07:58 +0300

    Add files via upload

--- EvaluationServlet.java
+++ EvaluationServlet.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.*;
 import javax.servlet.http.*;
 
 /**
@@ -15,100 +16,45 @@ public class EvaluationServlet extends HttpServlet {
     String resExtraversion; String resNeuroticism; String resLie;
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
+        List<String> q = new ArrayList<>();
+        for(int i = 0; i < 57; i++) {
+            q.add(req.getParameter("q"+(i+1)));
+        }
 
-        //Блок полученных ответов. Китайский код
-        String q1 = req.getParameter("q1");
-        String q2 = req.getParameter("q2");
-        String q3 = req.getParameter("q3");
-        String q4 = req.getParameter("q4");
-        String q5 = req.getParameter("q5");
-        String q6 = req.getParameter("q6");
-        String q7 = req.getParameter("q7");
-        String q8 = req.getParameter("q8");
-        String q9 = req.getParameter("q9");
-        String q10 = req.getParameter("q10");
-        String q11 = req.getParameter("q11");
-        String q12 = req.getParameter("q12");
-        String q13 = req.getParameter("q13");
-        String q14 = req.getParameter("q14");
-        String q15 = req.getParameter("q15");
-        String q16 = req.getParameter("q16");
-        String q17 = req.getParameter("q17");
-        String q18 = req.getParameter("q18");
-        String q19 = req.getParameter("q19");
-        String q20 = req.getParameter("q20");
-        String q21 = req.getParameter("q21");
-        String q22 = req.getParameter("q22");
-        String q23 = req.getParameter("q23");
-        String q24 = req.getParameter("q24");
-        String q25 = req.getParameter("q25");
-        String q26 = req.getParameter("q26");
-        String q27 = req.getParameter("q27");
-        String q28 = req.getParameter("q28");
-        String q29 = req.getParameter("q29");
-        String q30 = req.getParameter("q30");
-        String q31 = req.getParameter("q31");
-        String q32 = req.getParameter("q32");
-        String q33 = req.getParameter("q33");
-        String q34 = req.getParameter("q34");
-        String q35 = req.getParameter("q35");
-        String q36 = req.getParameter("q36");
-        String q37 = req.getParameter("q37");
-        String q38 = req.getParameter("q38");
-        String q39 = req.getParameter("q39");
-        String q40 = req.getParameter("q40");
-        String q41 = req.getParameter("q41");
-        String q42 = req.getParameter("q42");
-        String q43 = req.getParameter("q43");
-        String q44 = req.getParameter("q44");
-        String q45 = req.getParameter("q45");
-        String q46 = req.getParameter("q46");
-        String q47 = req.getParameter("q47");
-        String q48 = req.getParameter("q48");
-        String q49 = req.getParameter("q49");
-        String q50 = req.getParameter("q50");
-        String q51 = req.getParameter("q51");
-        String q52 = req.getParameter("q52");
-        String q53 = req.getParameter("q53");
-        String q54 = req.getParameter("q54");
-        String q55 = req.getParameter("q55");
-        String q56 = req.getParameter("q56");
-        String q57 = req.getParameter("q57");
-
-        String[] extraversionTrue = {q1, q3, q8, q10, q13, q17, q22, q25, q27, q39, q44, q46, q49, q53, q56};
-        String[] extraversionFalse = {q5, q15, q20, q29, q32, q34, q37, q41, q51};
-        String[] neuroticism = {q2, q4, q7, q9, q11, q14, q16, q19, q21, q23, q26, q28, q31, q33, q35, q38, q40, q43,
-                q45, q47, q50, q52, q55, q57};
-        String[] lieTrue = {q6, q24, q36};
-        String[] lieFalse = {q12, q18, q30, q42, q48, q54};
+        int[] extraversionTrue = {1, 3, 8, 10, 13, 17, 22, 25, 27, 39, 44, 46, 49, 53, 56};
+        int[] extraversionFalse = {5, 15, 20, 29, 32, 34, 37, 41, 51};
+        int[] neuroticism = {2, 4, 7, 9, 11, 14, 16, 19, 21, 23, 26, 28, 31, 33, 35, 38, 40, 43,
+                45, 47, 50, 52, 55, 57};
+        int[] lieTrue = {6, 24, 36};
+        int[] lieFalse = {12, 18, 30, 42, 48, 54};
 
         if (req.getContentLength() < 389) {
             resp.setCharacterEncoding("UTF-8");
             resp.getWriter().println(String.format(STANDARD_RESPONSE, "<p>Вы не ответили на все вопросы, попробуйте снова.<p>"));
         } else {
 
-            for (String i : extraversionTrue) {
-                if ("yes".equals(i)) {
+            for (int i : extraversionTrue) {
+                if ("yes".equals(q.get(i - 1))) {
                     pointExtraversion++;
                 }
             }
-            for (String i : extraversionFalse) {
-                if ("no".equals(i)) {
+            for (int i : extraversionFalse) {
+                if ("no".equals(q.get(i - 1))) {
                     pointExtraversion++;
                 }
             }
-            for (String i : neuroticism) {
-                if ("yes".equals(i)) {
+            for (int i : neuroticism) {
+                if ("yes".equals(q.get(i - 1))) {
                     pointNeuroticism++;
                 }
             }
-            for (String i : lieTrue) {
-                if ("yes".equals(i)) {
+            for (int i : lieTrue) {
+                if ("yes".equals(q.get(i - 1))) {
                     pointLie++;
                 }
             }
-            for (String i : lieFalse) {
-                if ("no".equals(i)) {
+            for (int i : lieFalse) {
+                if ("no".equals(q.get(i - 1))) {
                     pointLie++;
                 }
             }
@@ -159,6 +105,4 @@ public class EvaluationServlet extends HttpServlet {
             resp.getWriter().println(String.format(STANDARD_RESPONSE, res));
         }
     }
-
-
 }
